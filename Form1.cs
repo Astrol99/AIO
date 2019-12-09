6bff816bb2a65dfa4fb0794fc9b6d89f13d19bc9
David <dngames2006@gmail.com>
2019-12-08 18:02:21 -0600

    Made message boxes to either update offsets or not

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AIO
 {
@@ -15,6 +16,13 @@ namespace AIO
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // First things to do when window opens
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Check if offsets file exists
+            UpdateStuff.getOffsets();
         }
     }
 }
---
+++ Update.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AIO
+{
+    class UpdateStuff
+    {
+        public static void getOffsets()
+        {
+            String caption = "Offsets Updater";
+            MessageBoxButtons btn = MessageBoxButtons.YesNo;
+            MessageBoxIcon icon = MessageBoxIcon.Question;
+
+            // If file of offsets is present
+            if (!File.Exists("csgo.json"))
+            {
+                var result = MessageBox.Show("Offsets not found. Proceed to download offsets?", caption, btn, icon);
+                if (result == DialogResult.No) 
+                {
+                    MessageBox.Show("Offsets are required to run this program! Exiting...", caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+            }
+            else
+            {
+                var result = MessageBox.Show("Offsets found. Update offsets?", caption, btn, icon);
+            }
+        }
+    }
+}
